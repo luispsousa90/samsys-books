d4495baa1de3310309d9fa79c44883f67173ce30
Luis Sousa 1210854 <1210854@isep.ipp.pt>
2023-03-21 11:14:43 +0000

    [FE] React-table pagination is now working as intended.

--- books-app/src/components/Table/TableReact.tsx
+++ books-app/src/components/Table/TableReact.tsx
@@ -1,52 +1,87 @@
-import { useMemo } from 'react';
-import { Column, useTable } from 'react-table';
+import { useMemo, useEffect } from 'react';
+import { Column, useTable, usePagination } from 'react-table';
 import { Link } from 'react-router-dom';
 import Book from '../../types/Book/Book';
 
 interface ITableReactProps {
+  columns: Column<Book>[];
   data: Book[];
+  pageCount: number;
+  pageSize: number;
+  pageIndex: number;
+  loading: boolean;
+  fetchData: (pageSize: number, pageIndex: number) => void;
 }
 
 export default function TableReact(props: ITableReactProps) {
   const data = useMemo(() => props.data, [props.data]);
 
-  const columns = useMemo<Column<Book>[]>(
-    () => [
-      {
-        Header: 'ID',
-        accessor: 'id', // accessor is the "key" in the data
-      },
-      {
-        Header: 'ISBN',
-        accessor: 'isbn',
-      },
-      {
-        Header: 'Title',
-        accessor: 'name',
-      },
-      {
-        Header: 'Author',
-        accessor: 'authorName',
-      },
-      {
-        Header: 'Price',
-        accessor: 'price',
-      },
-    ],
-    []
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    rows, // rows are the data
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns: props.columns,
+      data,
+      initialState: { pageIndex: 0 },
+      manualPagination: true,
+      pageCount: props.pageCount,
+    },
+    usePagination
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+  useEffect(() => {
+    props.fetchData(pageIndex, pageSize);
+    // ignore the warning
+  }, [pageIndex, pageSize]);
 
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
+    <>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              pageIndex,
+              pageSize,
+              pageCount,
+              canNextPage,
+              canPreviousPage,
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+      <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: 'solid 3px red',
+                    background: 'aliceblue',
+                    color: 'black',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
               <th
-                {...column.getHeaderProps()}
                 style={{
                   borderBottom: 'solid 3px red',
                   background: 'aliceblue',
@@ -54,54 +89,88 @@ export default function TableReact(props: ITableReactProps) {
                   fontWeight: 'bold',
                 }}
               >
-                {column.render('Header')}
+                Actions
               </th>
-            ))}
-            <th
-              style={{
-                borderBottom: 'solid 3px red',
-                background: 'aliceblue',
-                color: 'black',
-                fontWeight: 'bold',
-              }}
-            >
-              Actions
-            </th>
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'papayawhip',
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
-              <td
-                style={{
-                  padding: '10px',
-                  border: 'solid 1px gray',
-                  background: 'papayawhip',
-                }}
-              >
-                <Link to={`/book/edit/${row.original.id}`}>Edit</Link>
-              </td>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '10px',
+                        border: 'solid 1px gray',
+                        background: 'papayawhip',
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+                <td
+                  style={{
+                    padding: '10px',
+                    border: 'solid 1px gray',
+                    background: 'papayawhip',
+                  }}
+                >
+                  <Link to={`/book/edit/${row.original.id}`}>Edit</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className='pagination'>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type='number'
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[3, 5, 10, 20, 25].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 }
--- books-app/src/pages/ReactTablePage.tsx
+++ books-app/src/pages/ReactTablePage.tsx
@@ -1,11 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Typography } from '@mui/material';
 import TableReact from '../components/Table/TableReact';
 import { getBooks } from '../services/BookService';
 import MainLayout from '../layouts/MainLayout';
+import { Column } from 'react-table';
+import Book from '../types/Book/Book';
 
 export default function ReactTablePage() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [pageCount, setPageCount] = useState<number>(0);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [pageIndex, setPageIndex] = useState<number>(0);
 
   useEffect(() => {
     (async () => {
@@ -14,12 +20,55 @@ export default function ReactTablePage() {
     })();
   }, []);
 
+  const fetchData = async (pageSize: number, pageIndex: number) => {
+    setLoading(true);
+    const res = await getBooks('', pageIndex, pageSize);
+    setBooks(res.obj.items);
+    const pageCount = Math.ceil(res.obj.totalCount / pageIndex);
+    setPageCount(pageCount);
+    setLoading(false);
+  };
+
+  const columns = useMemo<Column<Book>[]>(
+    () => [
+      {
+        Header: 'ID',
+        accessor: 'id', // accessor is the "key" in the data
+      },
+      {
+        Header: 'ISBN',
+        accessor: 'isbn',
+      },
+      {
+        Header: 'Title',
+        accessor: 'name',
+      },
+      {
+        Header: 'Author',
+        accessor: 'authorName',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+    ],
+    []
+  );
+
   return (
     <MainLayout>
       <Typography variant='h3' align='center' gutterBottom>
         Using react-table
       </Typography>
-      <TableReact data={books} />
+      <TableReact
+        columns={columns}
+        data={books}
+        pageCount={pageCount}
+        pageSize={pageSize}
+        pageIndex={pageIndex}
+        fetchData={fetchData}
+        loading={loading}
+      />
     </MainLayout>
   );
 }
